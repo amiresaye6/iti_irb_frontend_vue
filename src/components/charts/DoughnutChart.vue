8d70760d11a951b600a7a5dadb02f3376cd26e9f
Amir <amirmawla27@gmail.com>
2026-06-09 07:25:03 +0300

    feat : add another component for common chart

---
+++ src/components/charts/DoughnutChart.vue
@@ -0,0 +1,22 @@
+<template>
+  <Doughnut :data="chartData" :options="chartOptions" />
+</template>
+
+<script setup>
+import { Doughnut } from 'vue-chartjs';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+defineProps({
+  chartData: {
+    type: Object,
+    required: true,
+  },
+});
+
+const chartOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+};
+</script>
